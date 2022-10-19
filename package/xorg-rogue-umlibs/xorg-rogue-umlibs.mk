c1122fb2a6ec839cffe27050215bca2fd1b1ac71
weishanshan1084 <weishanshan1084@phytium.com.cn>
2022-10-19 16:38:31 +0800

    xorg-rogue-umlibs: change the url to gitee
    Signed-off-by: weishanshan1084 <weishanshan1084@phytium.com.cn>

--- package/xorg-rogue-umlibs/xorg-rogue-umlibs.mk
+++ package/xorg-rogue-umlibs/xorg-rogue-umlibs.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-XORG_ROGUE_UMLIBS_VERSION = 0ce3d27fe96b56e36db4e7d6db9d0e7fff737c8b
-XORG_ROGUE_UMLIBS_CUSTOM_REPO_URL = ssh://git@gitlab.phytium.com.cn:12022/embedded/linux/phytium-rogue-umlibs.git
+XORG_ROGUE_UMLIBS_VERSION = 213244f08503ce002cd816abbdc3e353f34e9a1b
+XORG_ROGUE_UMLIBS_CUSTOM_REPO_URL = https://gitee.com/phytium_embedded/phytium-rogue-umlibs.git
 XORG_ROGUE_UMLIBS_SITE = $(call qstrip,$(XORG_ROGUE_UMLIBS_CUSTOM_REPO_URL))
 XORG_ROGUE_UMLIBS_INSTALL_IMAGES = YES
 XORG_ROGUE_UMLIBS_SITE_METHOD = git
